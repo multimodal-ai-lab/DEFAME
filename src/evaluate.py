a425b83ac11372814127d9e8b8b56ec7c49fea4a
MaggiR <mark@rothermel.me>
2024-05-21 13:43:24 +0200

    Make evaluation output more informative

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,13 +2,14 @@ import numpy as np
 
 from safe.fact_checker import FactChecker
 from eval.benchmark import AVeriTeC, FEVER
+from common.console import green, red, bold
 
 
 model = "OPENAI:gpt-3.5-turbo-0125"
 # model = "huggingface:meta-llama/Meta-Llama-3-70B-Instruct"
 search_engine = "wiki"  # "google" or "wiki"
 benchmark = FEVER("dev")
-n = 5
+n = 20
 extract_claims = False
 
 
@@ -23,9 +24,13 @@ fc = FactChecker(model=model, search_engine=search_engine, extract_claims=extrac
 predictions = []
 for instance in benchmark:
     content = instance["content"]
-    prediction = fc.check(content,verbose=False)
+    prediction = fc.check(content, verbose=True)
     predictions.append(prediction)
-    if len(predictions) == 5:
+    if instance["label"] == prediction:
+        print(bold(green("CORRECT")))
+    else:
+        print(bold(red("WRONG - Ground truth: " + instance["label"].value)))
+    if len(predictions) == n:
         break
 
 # Compute metrics
