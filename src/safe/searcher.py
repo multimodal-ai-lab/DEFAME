41494e2851c9b2d1948ec0221adc027f070bac36
MaggiR <mark@rothermel.me>
2024-05-21 13:42:09 +0200

    Add search result summarization in searcher, rework query post-processing

--- src/safe/searcher.py
+++ src/safe/searcher.py
@@ -1,11 +1,12 @@
 import dataclasses
 from typing import Optional, Sequence
+import re
 
 from common import utils
 from common.modeling import Model
 from common.shared_config import serper_api_key
 from safe.config import num_searches, debug_safe, max_steps, max_retries
-from safe.prompts.prompt import SearchPrompt
+from safe.prompts.prompt import SearchPrompt, SummarizePrompt
 from safe.tools.query_serper import SerperAPI
 from safe.tools.wiki_dump import WikiDumpAPI
 
@@ -14,6 +15,9 @@ from safe.tools.wiki_dump import WikiDumpAPI
 class SearchResult:
     query: str
     result: str
+
+    def __str__(self):
+        return f"SearchResult(\n\tquery='{self.query}'\n\tresult='{self.result}'\n)"
 
 
 class Searcher:
@@ -39,8 +43,6 @@ class Searcher:
 
             while not next_search and num_tries <= self.max_retries:
                 next_search = self._maybe_get_next_search(claim, search_results, verbose=verbose)
-                if verbose:
-                    print("next_search: ", next_search)
                 num_tries += 1
 
             if next_search is None:
@@ -56,43 +58,68 @@ class Searcher:
                                past_searches: list[SearchResult],
                                verbose: Optional[bool] = False,
                                ) -> SearchResult | None:
-        """Get the next query from the model."""
+        """Get the next query from the model, use the query to search for evidence and return it."""
         # Construct the prompt tasking the model to produce a search query
-        knowledge = '\n'.join([s.result for s in past_searches if s.result is not None])
+        past_results = [s.result for s in past_searches if s.result is not None]
+        knowledge = '\n'.join(past_results)
         knowledge = 'N/A' if not knowledge else knowledge
-        past_queries = '\n'.join([s.query for s in past_searches])
-        past_queries = 'N/A' if not past_queries else past_queries
-        search_prompt = SearchPrompt(claim, knowledge, past_queries,
+        past_queries = [s.query for s in past_searches]
+        past_queries_str = '\n'.join(past_queries)
+        past_queries_str = 'N/A' if not past_queries_str else past_queries_str
+        search_prompt = SearchPrompt(claim, knowledge, past_queries_str,
                                      search_engine=self.search_engine,
                                      open_source=self.model.open_source)
 
         # Get and validate the model's response
         model_response = self.model.generate(str(search_prompt), do_debug=self.debug).replace('"', '')
-        if model_response.startswith("I cannot"):
-            if verbose:
-                utils.print_guard()
-            model_response = claim
-        query = utils.extract_first_code_block(model_response, ignore_language=True)
-        if not query:
-            query = self.post_process_query(model_response)
+        query = self.post_process_query(model_response)
 
         # Avoid casting the same, previously used query again
         if query in past_queries:
             return
 
-        return SearchResult(query=query, result=self._call_api(query))
+        result = self._call_api(query)
 
-    def post_process_query(self, model_response: str) -> str:
+        # Avoid duplicate results
+        if result in past_results:
+            result = None  # But keep query to avoid future duplicates
+
+        # If result is too long, summarize it (to avoid hitting the context length limit)
+        if result is not None and len(result) > 512:
+            print("Summarizing result:", result)
+            summarize_prompt = SummarizePrompt(query, result)
+            result = self.model.generate(str(summarize_prompt), do_debug=self.debug)
+
+        search_result = SearchResult(query=query, result=result)
+
+        if verbose:
+            print("Found", search_result)
+
+        return search_result
+
+    def post_process_query(self, model_response: str, verbose: bool = False) -> str:
         """
-        Processes the model response, ensures correct formatting, and adjusts the response if needed.
+        Processes the model response to extract the query. Ensures correct formatting
+        and adjusts the response if needed.
         """
-        # Check if there is no query in the model response
-        print("No query was found in output - likely due to wrong formatting.\nModel Output: {model_response}")
-        # Adjust the model response
-        adjustment_instruct = "Extract a simple sentence that I can use for a Google Search Query from this string:\n"
-        adjusted_model_response = self.model.generate(adjustment_instruct + model_response)
-        print(f'Extracted Query: {adjusted_model_response}')
-        return adjusted_model_response
+
+        if verbose and model_response.startswith("I cannot"):
+            utils.print_guard()
+
+        query = utils.extract_first_code_block(model_response, ignore_language=True)
+
+        # If query extraction was unsuccessful, use the LLM to extract the query from the response
+        if not query:
+            if verbose:
+                print(f"No query was found in output - likely due to wrong formatting.\nModel Output: {model_response}")
+            instruction = "Extract a simple sentence that I can use for a Google Search query from this string:\n"
+            query = self.model.generate(instruction + model_response)
+
+        # Remove unwanted newlines
+        query = query.replace('\n', '')
+        re.sub(r'[\n`´]', '', query)
+
+        return query
 
     def _call_api(self, search_query: str) -> str:
         """Call the respective search API to get the search result."""
