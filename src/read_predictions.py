1dbedc5be55369ce9caa24264a7ddfa12ee54ff2
MaggiR <mark@rothermel.me>
2024-06-12 18:19:47 +0200

    Bugfix read_predictions.py

--- src/read_predictions.py
+++ src/read_predictions.py
@@ -7,10 +7,11 @@ path = "insert_path"
 df = pd.read_csv(path)
 correct_stats = df["correct"].value_counts()
 prediction_stats = df["predicted"].value_counts()
-accuracy = correct_stats[True] / (len(df) - prediction_stats["REFUSED_TO_ANSWER"])
+n_refused = prediction_stats["REFUSED_TO_ANSWER"] if "REFUSED_TO_ANSWER" in list(prediction_stats.keys()) else 0
+accuracy = correct_stats[True] / (len(df) - n_refused)
 print("Accuracy: %.1f" % (accuracy * 100))
 plot_confusion_matrix(df["predicted"],
                       df["target"],
-                      classes=FEVER().get_classes(),
+                      classes=AVeriTeC().get_classes(),
                       benchmark_name="Averitec Dev",
                       save_dir=None)
