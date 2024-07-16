29055a6fafa74d896d1c67307bdfa59f61cfd4c2
TobiasBrx <tgb2117@columbia.edu>
2024-07-16 14:58:55 +0000

    deleting path_data to generalize and make it suitable for public

--- config/globals.py
+++ config/globals.py
@@ -8,8 +8,8 @@ search_engine_options = {
     'duckduckgo',
     'averitec_kb'
 }
-path_to_data = "/pfss/mlde/workspaces/mlde_wsp_Rohrbach/data/raw/"  # Where the datasets are stored
-path_to_result = "/pfss/mlde/workspaces/mlde_wsp_Rohrbach/users/mr74vahu/MAFC/out/"  # Where outputs are to be saved
+path_to_data = ""  # Where the datasets are stored
+path_to_result = ""  # Where outputs are to be saved
 embedding_model = "Alibaba-NLP/gte-base-en-v1.5"  # used for semantic search in FEVER and Averitec knowledge bases
 
 api_keys = yaml.safe_load(open("config/api_keys.yaml"))
