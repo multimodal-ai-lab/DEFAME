fc611a2464716ec8ab11655a5f9829867e9220b0
MaggiR <mark@rothermel.me>
2024-07-16 14:38:14 +0200

    Automate Averitec dataset and KB downloading/extraction for user convenience

--- scripts/build_averitec.py
+++ scripts/build_averitec.py
@@ -1,7 +1,9 @@
+"""Downloads and prepares the AVeriTeC knowledge base/store. Preparation
+will take several hours on an A100 GPU due to embedding computation."""
+
 from tools.search.knowledge_base import KnowledgeBase
-from config.globals import path_to_data
 
 
 if __name__ == '__main__':  # DB building uses multiprocessing
     kb = KnowledgeBase()
-    kb.build_db(path_to_data + "AVeriTeC/knowledge_dev/")
+    kb.build_db()
