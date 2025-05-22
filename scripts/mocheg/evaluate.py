aabe54ddea90caf63577d5a74af5f8500679c038
Marcus Kornmann <m-kornmann@gmx.de>
2025-05-22 10:27:35 +0200

    evaluate all samples

--- scripts/mocheg/evaluate.py
+++ scripts/mocheg/evaluate.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':  # evaluation uses multiprocessing
         llm_kwargs=dict(temperature=0.01),
         benchmark_name="mocheg",
         benchmark_kwargs=dict(variant=variant),
-        n_samples=1,
+        n_samples=None,
         sample_ids=None,
         print_log_level="info",
         random_sampling=True, # Set to True in MOCHEG as samples are ordered in the benchmark csv file according to their label.
