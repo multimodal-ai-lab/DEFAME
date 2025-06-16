1999a4fe31c0f7c669872341b250e190f604ead3
MaggiR <mark@rothermel.me>
2025-06-16 17:14:58 +0200

    Undo not needed changes

--- scripts/run.py
+++ scripts/run.py
@@ -4,8 +4,7 @@ from ezmm import Image
 
 from defame.fact_checker import FactChecker
 
-# fact_checker = FactChecker(llm="gpt_4o")
-fact_checker = FactChecker(llm="llama4_scout")
+fact_checker = FactChecker(llm="gpt_4o")
 claim = ["The image",
          Image("in/example/sahara.webp"),
          "shows the Sahara in 2023 covered with snow!"]
--- scripts/run_config.py
+++ scripts/run_config.py
@@ -2,4 +2,4 @@
 
 if __name__ == '__main__':
     from defame.eval.utils import run_experiment
-    run_experiment(config_file_path="config/cr/all_actions.yaml")
+    run_experiment(config_file_path="config/verite/default.yaml")
