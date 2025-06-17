b8a391b56e85a69a0bcc5e579b4e1640bfa37d5a
Marcus Kornmann <marcus.kornmann@icloud.com>
2025-06-17 14:53:55 +0200

    undo changes

--- config/globals.py
+++ config/globals.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Directories
 working_dir = Path.cwd() # working_dir should be DEFAME
-data_root_dir = Path("/pfss/mlde/workspaces/mlde_wsp_Shared_Datasets/")  # Where the datasets are stored
+data_root_dir = Path("data/")  # Where the datasets are stored
 result_base_dir = working_dir / "out/"  # Where outputs are to be saved
 temp_dir = result_base_dir / "temp/" # Where caches etc. are saved
 
@@ -17,7 +17,7 @@ api_keys = yaml.safe_load(open(api_key_path))
 
 google_service_account_key_path = Path("config/google_service_account_key.json")
 
-firecrawl_url = "http://thot.mai.informatik.tu-darmstadt.de:3002"  # applies to Firecrawl running in a 'firecrawl' Docker Container
+firecrawl_url = "http://firecrawl:3002"  # applies to Firecrawl running in a 'firecrawl' Docker Container
 
 random_seed = 42 # used for sub-sampling in partial dataset testing
 
