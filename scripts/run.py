4f5c5bdb0523452172c2ec3c3ce82a75d7058dcd
wgyhhhh <wgyhhh001@gmail.com>
2026-03-23 17:43:58 +0800

    feat: add Deepseek series model support

--- scripts/run.py
+++ scripts/run.py
@@ -4,7 +4,7 @@ from ezmm import Image
 
 from defame.fact_checker import FactChecker
 
-fact_checker = FactChecker(llm="gpt_4o")
+fact_checker = FactChecker(llm="deepseek_chat")
 claim = ["The image",
          Image("in/example/sahara.webp"),
          "shows the Sahara in 2023 covered with snow!"]
